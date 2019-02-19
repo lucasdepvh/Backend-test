db9f532f2e095ee8e4ddad72ae7daafa3463ff4b
André Oliveira <carlos.andre@sefin.ro.gov.br>
2019-02-19 07:57:25 -0400

    updating the schema to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,23 @@
 
 ActiveRecord::Schema.define(version: 2019_02_14_234513) do
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories_products", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "categories_products", id: false, force: :cascade do |t|
     t.bigint "product_id", null: false
     t.bigint "category_id", null: false
     t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
     t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id", unique: true
   end
 
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.datetime "created_at", null: false
